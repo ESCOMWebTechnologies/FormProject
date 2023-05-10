f0c1da7830e9635844de89a6da19e02bc5b9f7c1
RicardoBisogno <bisogno.ric@gmail.com>
2023-05-09 20:56:41 -0600

    update

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Dropdown,DropdownItem,DropdownMenu,DropdownToggle } from "reactstrap";
 
 function Home() {
+  const [dropDawn, setDropDawn] = useState(false);
+  const openDropDawn = () => {setDropDawn(!dropDawn)};
   return (
     <>
       <header>
@@ -9,9 +12,30 @@ function Home() {
           Easy Creator Forms
           <nav>
             <Link to={"/"}> Home</Link>
-            <Link to={"/CreateAccount"}> Create Acoount</Link>
-            <Link to={"/SignIn"}> Sing in</Link>
           </nav>
+          <Dropdown isOpen={dropDawn} toggle={openDropDawn}>
+            <DropdownToggle caret className="btn btn-light text-dark me-2">
+              <img
+                src="https://github.com/mdo.png"
+                alt="mdo"
+                width="32"
+                height="32"
+                class="rounded-circle"
+              />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>
+                <Link to={"/CreateAccount"}>Sign-up</Link>
+              </DropdownItem>
+              <DropdownItem>
+                <Link to={"/SignIn"}> Sign-in</Link>
+              </DropdownItem>
+              <DropdownItem divider/>
+              <DropdownItem>
+                <Link to={"/SignOut"}> Sign-out</Link>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </div>
       </header>
       <article>
