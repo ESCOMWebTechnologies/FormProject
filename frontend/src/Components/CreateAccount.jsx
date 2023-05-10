054c2efb67bc799c9c3ece8235ee89fe0e1c6c7e
RicardoBisogno <bisogno.ric@gmail.com>
2023-05-09 23:19:52 -0600

    update

--- frontend/src/Components/CreateAccount.jsx
+++ frontend/src/Components/CreateAccount.jsx
@@ -1,21 +1,32 @@
 import React, { useContext, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import { BsFillHouseDoorFill } from "react-icons/bs";
+import { GrFacebookOption, GrTwitter, GrInstagram } from "react-icons/gr";
 
 import { DataContext } from "../Context/DataContext.jsx";
 
 function CreateAccount() {
-  
   const [email, setEmail] = useState("");
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
-
   const { createAccount } = useContext(DataContext);
+
+  const [dropDawn, setDropDawn] = useState(false);
+  const openDropDawn = () => {
+    setDropDawn(!dropDawn);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password === password2) {
-      createAccount(email,user, password);
+      createAccount(email, user, password);
       setEmail("");
       setUser("");
       setPassword("");
@@ -31,16 +42,64 @@ function CreateAccount() {
   return (
     <>
       <Outlet />
-      <header>
-        <div>
-          Easy Creator Forms
-          <nav>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/CreateAccount"}>Create Acoount</Link>
-            <Link to={"/SignIn"}>Sing in</Link>
-          </nav>
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div className="col-md-3 mb-2 mb-md-0 ms-4">
+          <span className="fs-4">Easy Creator Forms</span>
+        </div>
+        <div className="col-md-3 text-end">
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <Link to={"/"} className="nav-link text-secondary">
+                <BsFillHouseDoorFill
+                  className="bi d-block mx-auto mb-1"
+                  size="25"
+                />
+                Home
+              </Link>
+            </li>
+            <li>
+              <Dropdown
+                isOpen={dropDawn}
+                toggle={openDropDawn}
+                className="flex-shrink-0 dropdown ms-1"
+              >
+                <DropdownToggle
+                  className="d-block link-dark text-decoration-none dropdown-toggle show"
+                  style={{ backgroundColor: "white", border: "0" }}
+                >
+                  <img
+                    src="https://github.com/mdo.png"
+                    alt="mdo"
+                    width="32"
+                    height="32"
+                    className="rounded-circle"
+                  />
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-menu text-small shadow">
+                  <DropdownItem header>Profile</DropdownItem>
+                  <DropdownItem>
+                    <Link to={"/CreateAccount"} className="dropdown-item">
+                      Sign-up
+                    </Link>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <Link to={"/SignIn"} className="dropdown-item">
+                      Sign-in
+                    </Link>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <Link to={"/SignOut"} className="dropdown-item">
+                      Sign-out
+                    </Link>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </li>
+          </ul>
         </div>
       </header>
+
       <article>
         <div>
           <form onSubmit={handleSubmit}>
@@ -94,6 +153,25 @@ function CreateAccount() {
           </form>
         </div>
       </article>
+
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <span className="mb-3 mb-md-0 text-body-secondary ms-3">
+            © 2023 Company, Inc
+          </span>
+        </div>
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="me-3">
+            <GrTwitter className="bi" size="25" />
+          </li>
+          <li className="me-3">
+            <GrInstagram className="bi" size="25" />
+          </li>
+          <li className="me-3">
+            <GrFacebookOption className="bi " size="25" />
+          </li>
+        </ul>
+      </footer>
     </>
   );
 }
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,47 +1,101 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Dropdown,DropdownItem,DropdownMenu,DropdownToggle } from "reactstrap";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import { BsFillHouseDoorFill } from "react-icons/bs";
+import { GrFacebookOption, GrTwitter, GrInstagram } from "react-icons/gr";
 
 function Home() {
   const [dropDawn, setDropDawn] = useState(false);
-  const openDropDawn = () => {setDropDawn(!dropDawn)};
+  const openDropDawn = () => {
+    setDropDawn(!dropDawn);
+  };
   return (
     <>
-      <header>
-        <div>
-          Easy Creator Forms
-          <nav>
-            <Link to={"/"}> Home</Link>
-          </nav>
-          <Dropdown isOpen={dropDawn} toggle={openDropDawn}>
-            <DropdownToggle caret className="btn btn-light text-dark me-2">
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="32"
-                height="32"
-                class="rounded-circle"
-              />
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>
-                <Link to={"/CreateAccount"}>Sign-up</Link>
-              </DropdownItem>
-              <DropdownItem>
-                <Link to={"/SignIn"}> Sign-in</Link>
-              </DropdownItem>
-              <DropdownItem divider/>
-              <DropdownItem>
-                <Link to={"/SignOut"}> Sign-out</Link>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div className="col-md-3 mb-2 mb-md-0 ms-4">
+          <span className="fs-4">Easy Creator Forms</span>
+        </div>
+        <div className="col-md-3 text-end">
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <Link to={"/"} className="nav-link text-secondary">
+                <BsFillHouseDoorFill
+                  className="bi d-block mx-auto mb-1"
+                  size="25"
+                />
+                Home
+              </Link>
+            </li>
+            <li>
+              <Dropdown
+                isOpen={dropDawn}
+                toggle={openDropDawn}
+                className="flex-shrink-0 dropdown ms-1"
+              >
+                <DropdownToggle
+                  className="d-block link-dark text-decoration-none dropdown-toggle show"
+                  style={{ backgroundColor: "white", border: "0" }}
+                >
+                  <img
+                    src="https://github.com/mdo.png"
+                    alt="mdo"
+                    width="32"
+                    height="32"
+                    className="rounded-circle"
+                  />
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-menu text-small shadow">
+                  <DropdownItem header>Profile</DropdownItem>
+                  <DropdownItem>
+                    <Link to={"/CreateAccount"} className="dropdown-item">
+                      Sign-up
+                    </Link>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <Link to={"/SignIn"} className="dropdown-item">
+                      Sign-in
+                    </Link>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <Link to={"/SignOut"} className="dropdown-item">
+                      Sign-out
+                    </Link>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </li>
+          </ul>
         </div>
       </header>
+
       <article>
         <div>Imagen de fondo y escritura</div>
       </article>
-      <footer>Descripcion de la compañia</footer>
+
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <span className="mb-3 mb-md-0 text-body-secondary ms-3">
+            © 2023 Company, Inc
+          </span>
+        </div>
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="me-3">
+            <GrTwitter className="bi" size="25" />
+          </li>
+          <li className="me-3">
+            <GrInstagram className="bi" size="25" />
+          </li>
+          <li className="me-3">
+            <GrFacebookOption className="bi " size="25" />
+          </li>
+        </ul>
+      </footer>
     </>
   );
 }
