be3f4a8d50194c97af2bc642af7c350a1e81a19f
RicardoBisogno <bisogno.ric@gmail.com>
2023-05-21 17:34:10 -0600

    update

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,170 +1,84 @@
-//Importacion del componente React y useState
-import React, { useState, useContext } from "react";
-
-//Importacion del componente Link que sirve para redirigir a los demas componentes con el browserRouter
-import { Link, Outlet, useLocation } from "react-router-dom";
-
-//Importacion de componentes para realizar un menu desplegable
+import React, { useState} from "react";
+import { Link } from "react-router-dom";
 import {
   Dropdown,
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
 } from "reactstrap";
-
-//Importacion de iconos de Bootstrap
 import {
   BsFillHouseDoorFill,
   BsFillSunFill,
   BsFillMoonFill,
   BsFillBrightnessAltHighFill,
 } from "react-icons/bs";
-
-//Importacion de iconos de Grommet-Icons
 import { GrFacebookOption, GrTwitter, GrInstagram } from "react-icons/gr";
 
-//Importacion del contexto
-import { DataContext } from "../Context/DataContext.jsx";
-
-//Se crea la funcion Home
 function Home() {
-  //Se llama a una variable del contexto
-  const { imagePath } = useContext(DataContext);
 
-  //Se crean los estados del menu desplegable para saber si esta abierto o cerrado
   const [dropDawn, setDropDawn] = useState(false);
   const [dropDawn2, setDropDawn2] = useState(false);
-
-  //Se crean las funciones para invertir los valores de abierto y cerrado del menu desplegable
   const openDropDawn = () => {
     setDropDawn(!dropDawn);
   };
   const openDropDawn2 = () => {
     setDropDawn2(!dropDawn2);
   };
-
-  //Elemento a cargar en el cuerpo de la pagina dependiendo de la ubicacvion del usuario
-  const { pathname } = useLocation();
-  const childsToRender = () => {
-    if (pathname === "/FormProject/") {
-      return (
-        <div className="container marketing mb-3 mt-5">
-          <div className="row me-5 ms-5">
-            <div className="col-lg-4">
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="100"
-                height="100"
-                className="bd-placeholder-img rounded-circle mb-"
-              />
-              <h2 className="fw-normal">Bisogno gandarilla Ricardo</h2>
-              <p>Front-End developer.</p>
-            </div>
-            <div className="col-lg-4">
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="100"
-                height="100"
-                className="bd-placeholder-img rounded-circle mb-3"
-              />
-              <h2 className="fw-normal">Ulrich Tamayo Daniel</h2>
-              <p>Back-End developer.</p>
-            </div>
-            <div className="col-lg-4">
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="100"
-                height="100"
-                className="bd-placeholder-img rounded-circle mb-3"
-              />
-              <h2 className="fw-normal">Ruiz Salgado Nayeli Adriana</h2>
-              <p>Database developer.</p>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return <Outlet />;
-    }
-  };
-
-  //cuando se manda a llamar al componente Home se retorna la pagina web
   return (
     <>
-      <header className="d-flex flex-wrap align-items-center justify-content-md-between justify-content-center border-bottom">
-        <div className="col-md-3 mb-md-0 ms-5 me-5 mb-3">
-          <span className="fs-6">Easy Creator Forms</span>
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div className="col-md-3 mb-2 mb-md-0 ms-4">
+          <span className="fs-4">Easy Creator Forms</span>
         </div>
-        <div className="col-md-5 text-end ms-5 mb-2 mt-2">
-          <ul className="nav col-12 col-md-auto justify-content-end">
-            <li className="me-4">
-              <Link
-                //to={"/"}
-                to={"/FormProject/"}
-                className="nav-link text-secondary"
-              >
+        <div className="col-md-3 text-end">
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <Link to={"/"} className="nav-link text-secondary">
                 <BsFillHouseDoorFill
                   className="bi d-block mx-auto mb-1"
-                  size="15"
+                  size="25"
                 />
-                <small>Home</small>
+                Home
               </Link>
             </li>
-            <li className="me-4">
+            <li>
               <Dropdown
                 isOpen={dropDawn}
                 toggle={openDropDawn}
-                direction="down"
+                className="flex-shrink-0 dropdown ms-1"
               >
                 <DropdownToggle
                   className="d-block link-dark text-decoration-none dropdown-toggle show"
                   style={{ backgroundColor: "white", border: "0" }}
                 >
                   <img
-                    src={imagePath}
+                    src="https://github.com/mdo.png"
                     alt="mdo"
-                    width="20"
-                    height="20"
+                    width="32"
+                    height="32"
                     className="rounded-circle"
                   />
                 </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-end shadow" end>
-                  <DropdownItem header>
-                    <small>Profile</small>
+                <DropdownMenu className="dropdown-menu text-small shadow">
+                  <DropdownItem header>Profile</DropdownItem>
+                  <DropdownItem>
+                    <Link to={"/CreateAccount"} className="dropdown-item">
+                      Sign-up
+                    </Link>
                   </DropdownItem>
                   <DropdownItem>
-                    <small>
-                      <Link
-                        //to={"/SignUp"}
-                        to={"/FormProject/SignUp/"}
-                        className="dropdown-item d-flex align-items-center"
-                      >
-                        Sign Up
-                      </Link>
-                    </small>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <small>
-                      <Link
-                        //to={"/LogIn"}
-                        to={"/FormProject/LogIn/"}
-                        className="dropdown-item d-flex align-items-center"
-                      >
-                        Log In
-                      </Link>
-                    </small>
+                    <Link to={"/SignIn"} className="dropdown-item">
+                      Sign-in
+                    </Link>
                   </DropdownItem>
                 </DropdownMenu>
               </Dropdown>
             </li>
-            <li className="me-4">
+            <li>
               <Dropdown
                 isOpen={dropDawn2}
                 toggle={openDropDawn2}
-                direction="down"
+                className="flex-shrink-0 dropdown ms-1"
               >
                 <DropdownToggle
                   className="d-block link-dark text-decoration-none dropdown-toggle show"
@@ -172,34 +86,18 @@ function Home() {
                 >
                   <BsFillBrightnessAltHighFill
                     className="rounded-circle"
-                    size="20"
+                    size="32"
                   />
                 </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-end shadow" end>
-                  <DropdownItem header>
-                    <small>Theme</small>
-                  </DropdownItem>
-                  <DropdownItem
-                    className="dropdown-item d-flex align-items-center"
-                    data-bs-theme-value="light"
-                    aria-pressed="false"
-                    disabled
-                  >
-                    <small>
+                <DropdownMenu className="dropdown-menu text-small shadow">
+                  <DropdownItem header>Theme</DropdownItem>
+                  <DropdownItem className="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
                       <BsFillSunFill className="bi me-2 opacity-50 theme-icon" />
-                      Light (Disabled)
-                    </small>
+                      Light
                   </DropdownItem>
-                  <DropdownItem
-                    className="dropdown-item d-flex align-items-center"
-                    data-bs-theme-value="dark"
-                    aria-pressed="false"
-                    disabled
-                  >
-                    <small>
+                  <DropdownItem className="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
                       <BsFillMoonFill className="bi me-2 opacity-50 theme-icon" />
-                      Dark (Disabled)
-                    </small>
+                     Dark
                   </DropdownItem>
                 </DropdownMenu>
               </Dropdown>
@@ -208,21 +106,25 @@ function Home() {
         </div>
       </header>
 
-      <article>{childsToRender()}</article>
+      <article>
+        <div>Imagen de fondo y escritura</div>
+      </article>
 
-      <footer className="d-flex flex-wrap align-items-center justify-content-md-between justify-content-center border-top">
-        <div className="col-md-3 mb-md-0 ms-5 me-5 mt-1">
-          <span className="text-body-secondary">© 2023 Company, Inc</span>
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <span className="mb-3 mb-md-0 text-body-secondary ms-3">
+            © 2023 Company, Inc
+          </span>
         </div>
-        <ul className="nav col-4 col-md-auto justify-content-end mt-1 me-4">
-          <li>
-            <GrTwitter className="bi" size="15" />
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="me-3">
+            <GrTwitter className="bi" size="25" />
           </li>
-          <li className="me-4 ms-4">
-            <GrInstagram className="bi" size="15" />
+          <li className="me-3">
+            <GrInstagram className="bi" size="25" />
           </li>
-          <li>
-            <GrFacebookOption className="bi " size="15" />
+          <li className="me-3">
+            <GrFacebookOption className="bi " size="25" />
           </li>
         </ul>
       </footer>
@@ -230,5 +132,4 @@ function Home() {
   );
 }
 
-//Se exporta el componente Home
 export default Home;
